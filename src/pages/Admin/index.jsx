f2b0211a078efe242f7a51c7208155bcbae09f28
Renan Vilela <renanvilelati@gmail.com>
2023-04-10 19:42:02 -0300

    Saving tasks on DB Firestore

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,17 +1,48 @@
-import { auth } from '../../firebaseConnection'
+import { auth, db } from '../../firebaseConnection'
 import { signOut } from 'firebase/auth';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { AdminContainer, Form } from "./styles";
+
+// Criando collection
+import { addDoc, collection } from 'firebase/firestore'
 
 export const Admin = () => {
 
     const [tarefaInput, setTarefaInput] = useState('')
+    const [user, setUser] = useState({})
 
-    function handleRegister(e) {
+    useEffect(() => {
+
+        async function loadTaks() {
+            const userDetails = localStorage.getItem('@userDetails')
+            setUser(JSON.parse(userDetails))
+        }
+
+        loadTaks()
+    }, [])
+
+    async function handleRegister(e) {
         e.preventDefault()
-        alert('teste')
+
+        if (tarefaInput.trim() === '') {
+            alert('Digite alguma tarefa')
+            return
+        }
+
+        await addDoc(collection(db, 'tarefas'), {
+            tarefa: tarefaInput,
+            created: new Date(),
+            userUid: user?.uid
+        })
+            .then(() => {
+                alert('TAREFA REGISTRADA');
+                setTarefaInput('')
+            })
+            .catch(error => {
+                console.error('ERRO AO REGISTRAR' + error);
+            })
     }
 
     async function handleLogout() {
