94996fd71864580f21b60ac193d1fe081d5b93b2
renanvilelati <renanvilelati@gmail.com>
2023-05-06 12:04:04 -0300

    refactor: Change error messages on the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { BrowserRouter } from "react-router-dom"
-import { RouterApp } from "./routes"
+import { BrowserRouter } from 'react-router-dom';
+import { RouterApp } from './routes';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <RouterApp />
+      <ToastContainer autoClose={3000} />
     </BrowserRouter>
-  )
-}
+  );
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,60 +1,59 @@
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { HomeContainer, Form } from './styles'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { HomeContainer, Form } from './styles';
 
-import { auth } from '../../firebaseConnection'
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../firebaseConnection';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 export const Home = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  async function handleLogin(e) {
+    e.preventDefault();
 
-    async function handleLogin(e) {
-        e.preventDefault()
-
-        if (email.trim() !== '' && password.trim() !== '') {
-
-            await signInWithEmailAndPassword(auth, email, password)
-                .then(() => {
-                    navigate('./admin', { replace: true })
-                })
-                .catch(error => {
-                    console.error('Error on log in');
-                })
-
-        } else {
-            alert('preencha os campos')
-
-        }
+    if (email.trim() !== '' && password.trim() !== '') {
+      await signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          navigate('./admin', { replace: true });
+        })
+        .catch((error) => {
+          toast.error('Email or password invalid');
+        });
+    } else {
+      toast.error('Please, enter your email and password');
     }
+  }
 
-    return (
-        <HomeContainer>
-            <h1>Task List</h1>
-            <span>Manage your schedule easily</span>
+  return (
+    <HomeContainer>
+      <h1>Task List</h1>
+      <span>Manage your schedule easily</span>
 
-            <Form onSubmit={handleLogin}>
-                <input
-                    type="text"
-                    placeholder='Enter your email'
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder='Enter your password'
-                    value={password}
-                    autoComplete='false'
-                    onChange={e => setPassword(e.target.value)}
-                />
+      <Form onSubmit={handleLogin}>
+        <input
+          type='text'
+          placeholder='Enter your email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='Enter your password'
+          value={password}
+          autoComplete='false'
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-                <button>Log in</button>
-            </Form>
+        <button>Log in</button>
+      </Form>
 
-            <Link to='/register' className='register-link'>Not a member yet? <span> Sign up for free </span></Link>
-        </HomeContainer>
-    )
-}
+      <Link to='/register' className='register-link'>
+        Not a member yet? <span> Sign up for free </span>
+      </Link>
+    </HomeContainer>
+  );
+};
