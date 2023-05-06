2142fe7f8b14c759548bacef90e10f3940ed7b8a
renanvilelati <renanvilelati@gmail.com>
2023-05-06 10:31:39 -0300

    refactor: change firebaseConnection variables

--- src/firebaseConnection.js
+++ src/firebaseConnection.js
@@ -1,22 +1,18 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-// Configuração pega no site do firebase para cada app
 const firebaseConfig = {
-    apiKey: "AIzaSyDN3l8Qo4eHpNYOIX4umjxkfNqvyE17Hfc",
-    authDomain: "curso-680af.firebaseapp.com",
-    projectId: "curso-680af",
-    storageBucket: "curso-680af.appspot.com",
-    messagingSenderId: "47313930713",
-    appId: "1:47313930713:web:4138616116834d4e4eb1c8"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
-// Inicializando o app
-const firebaseApp = initializeApp(firebaseConfig)
+const firebaseApp = initializeApp(firebaseConfig);
 
-// Recuperando o banco firestore
-export const db = getFirestore(firebaseApp)
+export const db = getFirestore(firebaseApp);
 
-// Fazer autenticação
-export const auth = getAuth(firebaseApp)
+export const auth = getAuth(firebaseApp);
